862f4c3eefb1cb69aa966c50a6e72aa36439a6b1
ETTech-52 <et.tech52@gmail.com>
2025-05-10 16:12:54 +0600

    Refactor account.js: Remove unused dropdown functionality to improve code clarity and maintainability

--- js/account.js
+++ js/account.js
@@ -1,3 +1,4 @@
+
 // Tab switching functionality
 document.querySelectorAll('.nav-item[data-tab]').forEach(tab => {
     tab.addEventListener('click', function (e) {
@@ -21,28 +22,3 @@ document.querySelectorAll('.nav-item[data-tab]').forEach(tab => {
         document.getElementById(tabId).classList.add('active');
     });
 });
-
-
-
-
-
-//* When the user clicks on the button, 
-// toggle between hiding and showing the dropdown content
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-    console.log("clicked")
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}
